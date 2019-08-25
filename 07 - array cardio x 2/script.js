5f0c053cad7891a554e5cfa4134b963cdbf10ee6
Alyssa Jurgens <aljurgens@yahoo.com>
2019-08-24 19:16:30 -0600

    Add some(), every(), find(), findIndex()

--- 07 - array cardio x 2/script.js
+++ 07 - array cardio x 2/script.js
@@ -12,3 +12,28 @@ const comments = [
   { text: 'Ramen is my fav food ever', id: 123523 },
   { text: 'Nice Nice Nice!', id: 542328 }
 ]
+
+// array.prototype.some() // if at least one person is 19 or older
+const isAdult = people.some(person => (new Date()).getFullYear() - person.year >= 19);
+
+// array.prototype.every() // if every person is 19 or older
+const allAdults = people.every(person => (new Date()).getFullYear() - person.year >= 19);
+
+// array.prototype.find() // like filter but just returns the first result
+const comment = comments.find(comment => comment.id === 823423);
+
+// array.prototype.findIndex() // find index of result
+const index = comments.findIndex(comment => comment.id === 823423);
+
+const newComments = [
+  ...comments.slice(0, index),
+  ...comments.slice(index + 1)
+];
+
+console.log({isAdult});
+console.log({allAdults});
+console.log({comment});
+console.log({index});
+
+console.table(comments);
+console.table(newComments);

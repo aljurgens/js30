e586e73f4ba162fcdc1a61d361e397bcce211f70
Alyssa Jurgens <aljurgens@yahoo.com>
2019-10-13 09:22:37 -0600

    Finish exercises

--- 09 - dev tools domination/script.js
+++ 09 - dev tools domination/script.js
@@ -7,13 +7,64 @@ function makeGreen() {
 }
 
 // Regular
+console.log('hello');
 
 // Interpolated
+console.log('Hello I am a %s string!', '💩');
 
 // Styled
+//console.log('%c I am some great text', 'font-size: 30px; background:red; text-shadow: 10px 10px 0 blue');
 
 // warning!
+console.warn('OH NOOO');
 
 // Error :/
+console.error('Shit!');
 
 // Info
+console.info('Crocodiles eat 3-4 people per year');
+
+// Testing
+const p = document.querySelector('p');
+console.assert(p.classList.contains('ouch'), 'That is wrong!');
+
+// Clearing
+console.clear();
+
+// Viewing DOM elements
+console.log(p);
+console.dir(p);
+
+console.clear();
+
+// Grouping together
+dogs.forEach(dog => {
+  console.groupCollapsed(`${dog.name}`);
+  console.log(`This is ${dog.name}`);
+  console.log(`${dog.name} is ${dog.age} years old`);
+  console.log(`${dog.name} is ${dog.age * 7} dog years old`);
+  console.groupEnd(`${dog.name}`);
+})
+
+// Counting
+console.count('Jay');
+console.count('Jay');
+console.count('Steve');
+console.count('Jay');
+console.count('Jay');
+console.count('Steve');
+console.count('Steve');
+console.count('Steve');
+console.count('Steve');
+
+// Timing
+console.time('fetching data');
+fetch('https://api.github.com/users/aljurgens')
+  .then(data => data.json())
+  .then(data => {
+    console.timeEnd('fetching data')
+    console.log(data);
+  })
+
+// Tables
+console.table(dogs);
